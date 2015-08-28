62d9aa9ee137e6739ce0fa7edcceb62c06456f66
yosurfer2 <lorenzo.camaione@gmail.com>
2015-08-28 15:08:21 +0200

    Carrierwave ready

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ActiveRecord::Base
+	mount_uploader :image, ImageUploader
+	
 	# Include default devise modules. Others available are:
 	# :confirmable, :lockable, :timeoutable and :omniauthable
 	devise :database_authenticatable, :registerable, :confirmable,
